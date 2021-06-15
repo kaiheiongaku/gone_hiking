910ee0df31ef4e9698edc53a23ca770e9122f759
Robert Heath <robert.b.heathii@gmail.com>
2021-06-15 16:19:28 -0500

    Create Park Poro

---
+++ app/poros/park.rb
@@ -0,0 +1,15 @@
+class Park
+  attr_reader :nps_id, :full_name, :description,  :phone_number, :entrance_fee,
+              :hours,  :image,     :weather_info, :office_address
+  def initialize(park_data)
+    @nps_id          = park_data[:id]
+    @full_name       = park_data[:full_name]
+    @description     = park_data[:description]
+    @phone_number    = park_data[:contacts][:phoneNumbers].first[:phoneNumber]
+    @entrance_fee    = park_data[:entranceFees].first[:cost]
+    @hours           = OperatingHours.new(park_data[:operatingHours])
+    @office_address  = Address.new( park_data[:addresses].first)
+    @image           = Image.new( park_data[:images].first)
+    @weather_info    = park_data[:weatherInfo]
+  end
+end
