77ad38b86d817fccc45193523569a5e0b4157a82
Robert Heath <robert.b.heathii@gmail.com>
2021-06-15 15:15:32 -0500

    Pushed some tests to facade. Checking to ensure the raw data is being returned from the nps api

--- spec/services/parks_service_spec.rb
+++ spec/services/parks_service_spec.rb
@@ -2,28 +2,28 @@ require 'rails_helper'
 
 describe ParksService do
   describe 'returns park information' do
-    it 'can return all parks with their information' do
+    it 'can return all parks with their information', :vcr do
       endpoint = '/api/v1/parks'
 
       parks = ParksService.call_parks_db(endpoint)
 
       expect(parks).to be_a(Hash)
-      expect(parks[:data].keys.count).to eq(872)
+      expect(parks[:data].count).to eq(50)
 
       park = parks[:data].first
       expect(park).to be_a(Hash)
-      expect(park.keys).to eq(10)
-      expect(park).to have_keys(:nps_id, :full_name, :description, :phone_number, :entrance_fee, :hours, :address, :image, :weather_info)
-
-      expect(park[:hours]).to have_keys(:description, :monday, :tuesday, :wednesday, :thursday, :friday, :saturday, :sunday)
-
-      expect(park[:address]).to have_keys(:postal_code, :city, :state, :street)
-
-      expect(park[:image]).to have_keys(:credit, :title, :description, :caption, :url)
+      expect(park.keys.count).to eq(23)
+      expect(park).to have_key(:id)
     end
 
-    it 'can return all parks within a state' do
+    it 'can return all parks within a state', :vcr do
+      endpoint = '/api/v1/parks'
+      params = { stateCode: 'wv' }
 
+      parks = ParksService.call_parks_db(endpoint, params)
+
+      expect(parks).to be_a(Hash)
+      expect(parks[:data].count).to eq(9)
     end
   end
 end
