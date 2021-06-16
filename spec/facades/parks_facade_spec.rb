7acde1a3f0516b2fb483916ea9499f3cbd7c6330
Robert Heath <robert.b.heathii@gmail.com>
2021-06-15 21:09:40 -0500

    Update tests and write first version of facade for parks. Consider refactoring.

--- spec/facades/parks_facade_spec.rb
+++ spec/facades/parks_facade_spec.rb
@@ -2,14 +2,14 @@ require 'rails_helper'
 
 describe ParksFacade do
   describe 'parks' do
-    it 'returns park objects without parameters' do
+    it 'returns park objects without parameters', :vcr do
       parks = ParksFacade.pull_parks_info
 
       expect(parks).to be_an(Array)
       expect(parks.first).to be_a(Park)
     end
 
-    it 'returns park objects with state parameters' do
+    it 'returns park objects with state parameters', :vcr do
       parks = ParksFacade.pull_parks_info('wv')
 
       expect(parks).to be_an(Array)
