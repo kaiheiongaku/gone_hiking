eff1c8d747ebc4d94c424181afb6db9e812b31df
Robert Heath <robert.b.heathii@gmail.com>
2021-06-15 20:06:24 -0500

    Update park PORO spec to adjust for hours, address, and image poros

--- spec/poros/park_spec.rb
+++ spec/poros/park_spec.rb
@@ -4,41 +4,25 @@ describe Park do
   describe 'park creation' do
     it 'is a park object with attributes' do
       park_file = File.read('spec/fixtures/poros/park_spec.json')
-
-      park_data = JSON.parse(background_file, symbolize_names: true)
-
+      park_data = JSON.parse(park_file, symbolize_names: true)
       park = Park.new(park_data)
 
       expect(park).to be_a(Park)
 
-      expect(park.nps_id).to eq()
-      expect(park.full_name)
-      expect(park.description).to eq()
-      expect(park.phone_number).to eq()
-      expect(park.entrance_fee).to eq()
+      expect(park.nps_id).to eq('FAEF5684-83A4-4CF2-A701-60CF8D4014BD')
+      expect(park.full_name).to eq('Appalachian National Scenic Trail')
+      expect(park.description).to eq(
+        "The Appalachian Trail is a 2,180+ mile long public footpath that traverses the scenic, wooded, pastoral, wild, and culturally resonant lands of the Appalachian Mountains. Conceived in 1921, built by private citizens, and completed in 1937, today the trail is managed by the National Park Service, US Forest Service, Appalachian Trail Conservancy, numerous state agencies and thousands of volunteers."
+      )
+      expect(park.phone_number).to eq('3045356278')
+      expect(park.entrance_fee).to eq('0.00')
+      expect(park.weather_info).to eq(
+        "It is your responsibility to be prepared for all weather conditions, including extreme and unexpected weather changes year-round. As the trail runs from Georgia to Maine there will be different weather conditions depending on your location. For weather along specific sections of the trail and at specific shelters, please refer to: http://www.atweather.org/"
+      )
 
-      expect(park.hours).to be_a(Hash)
-      expect(park.hours).to have_keys(:description, :monday, :tuesday, :wednesday, :thursday, :friday, :saturday, :sunday)
-      #hours object?
-      expect(park.address).to be_a(Hash)
-      expect(park.address).to have_keys(:postal_code, :city, :state, :street)
-      #object?
-      expect(park.image).to be_a(Hash)
-      expect(park.image).to have_keys(:credit, :title, :description, :caption, :url)
-
-      #object?
-      expect(park.weather_info).to eq()
-
-
-      # :nps_id,
-      # :full_name, :description, :phone_number, :entrance_fee, :hours, :address, :image, :weather_info)
-
-      # expect(park[:hours]).to have_keys(:description, :monday, :tuesday, :wednesday, :thursday, :friday, :saturday, :sunday)
-
-      # expect(park[:address]).to have_keys(:postal_code, :city, :state, :street)
-      #
-      # expect(park[:image]).to have_keys(:credit, :title, :description, :caption, :url)
-
+      expect(park.hours).to be_an(OperatingHours)
+      expect(park.office_address).to be_an(Address)
+      expect(park.image).to be_an(Image)
     end
   end
 end
