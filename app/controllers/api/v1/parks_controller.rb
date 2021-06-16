b29cd3254f23f00199110f56325885f8c0fbbe2e
Robert Heath <robert.b.heathii@gmail.com>
2021-06-16 13:51:27 -0500

    Update parks controller to check for presence of state code param and move errors to application controller

--- app/controllers/api/v1/parks_controller.rb
+++ app/controllers/api/v1/parks_controller.rb
@@ -1,7 +1,20 @@
 class Api::V1::ParksController < ApplicationController
+  before_action :validate_state_code, if: :state_code_present?
+
   def index
-  render json: ParksSerializer.new(
-      ParksFacade.pull_parks_info(params[:state])
+      render json: ParksSerializer.new(
+      @parks
     )
+  end
+
+  def state_code_present?
+    if params[:state]
+      params[:state].delete!('.') if params[:state].include?('.')
+      @parks = ParksFacade.pull_parks_info(params[:state])
+      true
+    else
+      @parks = ParksFacade.pull_parks_info
+      false
+    end
   end
 end
