f8a3740b415d63e86f7e39a604bdb1c7a4b364ad
Robert Heath <robert.b.heathii@gmail.com>
2021-06-16 14:35:19 -0500

    Adjust lines for readability

--- app/poros/park.rb
+++ app/poros/park.rb
@@ -5,11 +5,16 @@ class Park
     @nps_id          = park_data[:id]
     @full_name       = park_data[:fullName]
     @description     = park_data[:description]
-    @phone_number    = park_data[:contacts][:phoneNumbers].first[:phoneNumber] unless park_data[:contacts][:phoneNumbers] == []
-    @entrance_fee    = park_data[:entranceFees].first[:cost] unless park_data[:entranceFees].first == nil
-    @hours           = OperatingHours.new(park_data[:operatingHours].first) unless park_data[:operatingHours].first == nil
-    @office_address  = Address.new(park_data[:addresses].first) unless park_data[:addresses].first == nil
-    @image           = Image.new( park_data[:images].first) unless park_data[:images].first == nil
+    @phone_number    = park_data[:contacts][:phoneNumbers].first[:phoneNumber]
+                       unless park_data[:contacts][:phoneNumbers] == []
+    @entrance_fee    = park_data[:entranceFees].first[:cost]
+                       unless park_data[:entranceFees].first == nil
+    @hours           = OperatingHours.new(park_data[:operatingHours].first)
+                       unless park_data[:operatingHours].first == nil
+    @office_address  = Address.new(park_data[:addresses].first)
+                       unless park_data[:addresses].first == nil
+    @image           = Image.new( park_data[:images].first)
+                       unless park_data[:images].first == nil
     @weather_info    = park_data[:weatherInfo]
   end
 end
