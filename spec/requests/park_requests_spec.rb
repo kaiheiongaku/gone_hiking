91e3484795e756c8f17dbc263770091e95b4c184
Robert Heath <robert.b.heathii@gmail.com>
2021-06-16 09:47:57 -0500

    Rewrite tests for pulling parks for states and all parks so that it reflects most recent data return decisions

--- spec/requests/park_requests_spec.rb
+++ spec/requests/park_requests_spec.rb
@@ -2,22 +2,46 @@ require 'rails_helper'
 
 describe 'park information requests' do
   describe 'happy path' do
-    it 'can pull all parks for a state' do
+    it 'can pull all parks for a state', :vcr do
       get '/api/v1/parks?state=wv'
 
       expect(response).to be_successful
 
       parks_in_wv_json = JSON.parse(response.body, symbolize_names: true)[:data]
 
-      expect(parks_in_wv_json).to be_a(Hash)
-      expect(parks_in_wv_json).to have_key(:id)
-      expect(parks_in_wv_json[:id]).to eq('null')
+      expect(parks_in_wv_json).to be_a(Array)
+      expect(parks_in_wv_json.count).to eq(9)
 
-      ## test for speific attributes, test for number of parks, and that each park has specific attributes
+      expect(parks_in_wv_json.first).to have_key(:id)
+      expect(parks_in_wv_json.first[:id]).to eq('null')
+
+      expect(parks_in_wv_json.first).to have_key(:type)
+      expect(parks_in_wv_json.first[:type]).to eq('parks')
+
+      expect(parks_in_wv_json.first).to have_key(:attributes)
+      expect(parks_in_wv_json.first[:attributes]).to be_a(Hash)
+      expect(parks_in_wv_json.first[:attributes].keys.count).to eq(9)
     end
 
-    # it 'can show information about a particular park' do
-    #   visit '/api/v1/parks?state=WV'
-    # end
+    it 'can pull all parks', :vcr do
+      get '/api/v1/parks'
+
+      expect(response).to be_successful
+
+      all_parks = JSON.parse(response.body, symbolize_names: true)[:data]
+
+      expect(all_parks).to be_a(Array)
+      expect(all_parks.count).to eq(50)
+
+      expect(all_parks.first).to have_key(:id)
+      expect(all_parks.first[:id]).to eq('null')
+
+      expect(all_parks.first).to have_key(:type)
+      expect(all_parks.first[:type]).to eq('parks')
+
+      expect(all_parks.first).to have_key(:attributes)
+      expect(all_parks.first[:attributes]).to be_a(Hash)
+      expect(all_parks.first[:attributes].keys.count).to eq(9)
+    end
   end
 end
