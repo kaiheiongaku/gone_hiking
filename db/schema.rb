5555ea6de9e257b71a8871bee77675d1d0f85e94
Robert Heath <robert.b.heathii@gmail.com>
2021-06-16 15:33:01 -0500

    Update users schema to switch username for email

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_16_200935) do
+ActiveRecord::Schema.define(version: 2021_06_16_202013) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,11 +22,11 @@ ActiveRecord::Schema.define(version: 2021_06_16_200935) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "username"
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "api_key"
+    t.string "email"
   end
 
 end
