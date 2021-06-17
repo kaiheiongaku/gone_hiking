9cb582be3a8e791e60ad87af87b4c02bd5c9dbf1
Robert Heath <robert.b.heathii@gmail.com>
2021-06-17 11:32:16 -0500

    Update routes for search creation

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :parks, only: :index
       resources :users, only: :create
+      resources :searches, only: :create
     end
   end
   match '*unmatched_route', :to => 'application#raise_not_found!', :via => :all
