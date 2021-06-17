9cf379eeaf6d9f254d3242005dbabbb9b54cdbfa
Robert Heath <robert.b.heathii@gmail.com>
2021-06-17 10:53:02 -0500

    Update search and tests so that uniqueness is scoped to the user

--- app/models/search.rb
+++ app/models/search.rb
@@ -2,4 +2,5 @@ class Search < ApplicationRecord
   belongs_to :user
 
   validates_presence_of :user, :text, required: true
+  validates_uniqueness_of :text, scope: :user_id
 end
--- spec/models/search_spec.rb
+++ spec/models/search_spec.rb
@@ -8,5 +8,6 @@ describe Search do
   describe 'validations' do
     it { should validate_presence_of(:user) }
     it { should validate_presence_of(:text) }
+    it { should validate_uniqueness_of(:text).scoped_to(:user_id) }
   end
 end
