abb97275c199dbd39fdbd63815034ffcde8807f4
Robert Heath <robert.b.heathii@gmail.com>
2021-06-17 10:41:41 -0500

    Test and add text/user validation for search

--- app/models/search.rb
+++ app/models/search.rb
@@ -1,3 +1,5 @@
 class Search < ApplicationRecord
   belongs_to :user
+
+  validates_presence_of :user, :text, required: true
 end
--- spec/models/search_spec.rb
+++ spec/models/search_spec.rb
@@ -4,4 +4,9 @@ describe Search do
   describe 'relationships' do
     it { should belong_to(:user) }
   end
+
+  describe 'validations' do
+    it { should validate_presence_of(:user) }
+    it { should validate_presence_of(:text) }
+  end
 end
